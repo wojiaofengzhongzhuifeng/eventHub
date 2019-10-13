564b6f3b24aff163867be7a499fa55498c58a4cd
饶家俊 <raojiajun@bimwinner.com>
2019-10-13 22:47:12 +0800

    error message

--- test/index.ts
+++ test/index.ts
@@ -4,7 +4,7 @@ import Event from '../src/index';
 {
 //
   let event = new Event();
-  console.assert(event instanceof Event, 'new EventHub 能生成一个实例');
+  console.assert(event instanceof Event, 'new EventHub 不能生成一个实例');
 }
 
 {
@@ -17,6 +17,6 @@ import Event from '../src/index';
   event.emit('click111', 'data1');
   // setTimeout + 最后执行 assert，或者得到 called 的最终值
   setTimeout(()=>{
-    console.assert(called, 'on 注册的函数被调用了')
+    console.assert(called, 'on 注册的函数未被调用了')
   }, 500)
 }
