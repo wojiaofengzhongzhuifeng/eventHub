d084fa4b16483e13c02fe22779560f10f63f6dc6
饶家俊 <raojiajun@bimwinner.com>
2019-10-13 21:21:35 +0800

    确定所需数据结构

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 class EventHub{
+  /*
+  * events: {
+  *   click1: [fn1, fn2, fn3],
+  *   click2: [fn1, fn2, fn3],
+  * }
+  * */
   events: {};
   on(eventName, fn){};
   emit(eventName, data){};
